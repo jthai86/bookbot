efc7392b7e631288da49409e6f8546deee4a0e30
jthai <jthai@gmail.com>
2025-06-27 13:51:11 -0700

    adding a book reader

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-print("greetings boots")
+import sys
+from stats import get_num_words, get_chars_dict, chars_dict_to_sorted_list
 
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    sorted_chars = chars_dict_to_sorted_list(chars_dict)
+
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+
+def get_book_text(path):
+    with open(path, encoding="utf-8") as f:
+        return f.read()
+
+main()
